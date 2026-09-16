7cfdbee87c173efcbbe5af869882586aa1b989b9
Williams Adele <williams.adele.dev@gmail.com>
2026-09-16 14:35:15 +0100

    creating function that renders list to the dom

--- script.js
+++ script.js
@@ -3,20 +3,35 @@ const moneyIncome = document.getElementById("money-plus");
 const moneyExpense = document.getElementById("money-minus");
 const inputText = document.getElementById("text");
 const inputAmount = document.getElementById("amount");
-const addBtn = document.getElementById("btn");
+const form = document.getElementById("form");
+const hitsoryEl = document.getElementById("list");
 let transactions = []
 
-function addTransaction() {
-    event.preventDefault
+form.addEventListener("submit", addTransaction)
+function addTransaction(event) {
+    event.preventDefault();
     if(inputText.value.trim() === "" || inputAmount.value.trim() === ""){
-        alert = "Please input both name and amount, idiot"
+        alert("Please input both name and amount, idiot")
         return ""
     }
     let transaction = {
-        id: Date.now,
+        id: Date.now(),
         text: inputText.value.trim(),
         amount: Number(inputAmount.value.trim())
     }
     transactions.push(transaction)
+    render()
+    inputText = ""; inputAmount = ""
 }
-addBtn.addEventListener("submit", addTransaction)
+function render () {
+    let entry = ""
+    for(let i = 0; i<transactions.length; i++){
+        const sign = transactions[i].amount > 0 ? "+" : "-"
+        const itemClass = transactions[i].amount > 0 ? "plus" : "minus"
+     entry += `<li class="${itemClass}>
+    ${transactions[i].text}
+    <span>${sign}$${Math.abs(transactions[i].amount)}</span>
+    </li>`
+    }
+    historyEl.innerHTML = entry
+}
